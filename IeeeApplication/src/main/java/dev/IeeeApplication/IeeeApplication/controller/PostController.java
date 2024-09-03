c2725e6043ba638075c803e38bece70448b59fea
Yassine Ben Ayed <104756384+yassine0010@users.noreply.github.com>
2024-09-03 11:43:08 +0100

    PostController

--- IeeeApplication/src/main/java/dev/IeeeApplication/IeeeApplication/controller/PostController.java
+++ IeeeApplication/src/main/java/dev/IeeeApplication/IeeeApplication/controller/PostController.java
@@ -3,7 +3,6 @@ package dev.IeeeApplication.IeeeApplication.controller;
 import dev.IeeeApplication.IeeeApplication.Entity.PostEntity;
 import dev.IeeeApplication.IeeeApplication.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,15 +15,11 @@ public class PostController {
     @Autowired
     private PostRepository postRepository;
 
-    @PostMapping
+    @PostMapping("/Posting")
     public ResponseEntity<PostEntity> postMethod(@RequestBody PostEntity postEntity) {
-        try {
-            PostEntity savedPost = postRepository.save(postEntity);
-            return ResponseEntity.ok(savedPost);
-        } catch (Exception exception) {
-            // Log the exception or handle it appropriately
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
+        PostEntity savedPost = postRepository.save(postEntity);
+        return ResponseEntity.ok(savedPost);
+
     }
 
 
