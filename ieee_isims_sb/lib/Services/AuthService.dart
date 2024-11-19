45b6f3a06f4c0d2fb4b0eb12900f5a9a920d9276
Yassine Ben Ayed <104756384+yassine0010@users.noreply.github.com>
2024-11-19 20:43:17 +0100

    ?

--- ieee_isims_sb/lib/Services/AuthService.dart
+++ ieee_isims_sb/lib/Services/AuthService.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
-import 'package:ieee_isims_sb/Services/Notification/GetTokenService.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 Future<bool> login(
